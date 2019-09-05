1f72a36a7c86529ad7930c0dad1203129679ec23
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2019-09-05 15:08:18 -0300

    initial components structure

---
+++ src/components/layout/Main.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Main extends Component {
+
+    state = {
+
+
+    }
+
+    render() {
+
+        return(
+
+            <p>Main Component</p>
+        )
+    }
+
+}
+
+export default Main
