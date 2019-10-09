07ddaa7da1a33e33d0f8b9e745b06c95e1ea8b00
rodrigogomesrc <rocha.rodrigogomes@gmail.com>
2019-10-09 13:47:10 -0300

    sortear number validations complete

--- src/components/pages/Sorteio.js
+++ src/components/pages/Sorteio.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import './Sorteio.css';
 
-export default class Sorteio extends React.Component {
+export default class Sorteio extends Component {
 
 
     state = {
@@ -13,7 +13,8 @@ export default class Sorteio extends React.Component {
         numbersSorteados : [],
         lettersSorteadas : [],
         noRepeatError: false,
-        rangeError: false
+        rangeError: false,
+        valueError: false
     }
 
     randomLetters = () => {
@@ -24,32 +25,20 @@ export default class Sorteio extends React.Component {
 
     randomNumbers = () => {
 
-        const {start, end, quantity, unique} = this.state;
+        let {start, end, quantity, unique} = this.state;
         let numbersSorteados = this.state.numbersSorteados;
-        let noRepeatError = this.state.noRepeatError;
-        let rangeError = this.state.rangeError;
         let randomNumber = null;
         numbersSorteados = [];
+        quantity = parseInt(quantity);
+        start = parseInt(start);
+        end = parseInt(end);
 
-        // move those validations to the handle input functions
-        // make null values as default and make an error in case they are not provided
-        if(unique == "yes" && (end - start + 1) < quantity ){
+        if(!this.validate()) {
 
-            this.setState({noRepeatError: true});
-            noRepeatError = true;
-        }
-        if(start > end || start === end){
-
-            this.setState({rangeError: true});
-            rangeError = true;
-
-        } else {
-
-            rangeError = false;
-            noRepeatError = false;
-            this.setState({numbersSorteados, noRepeatError: false}, () => {
-
-                if(unique == "yes") {
+            console.log("calculando...");
+            this.setState({rangeError: false, noRepeatError: false, valueError: false}, () => {
+            
+                if(unique === "yes") {
     
                     for(var i = 0; i < quantity; i++){
                     
@@ -57,60 +46,149 @@ export default class Sorteio extends React.Component {
         
                             randomNumber = Math.floor(Math.random() * (end + 1 - start));
                             randomNumber += start;
+                            //if the number is not on the list it pushes to the list
                             if(numbersSorteados.indexOf(randomNumber) === -1){
                                 break
                             }
                         }
-                        
+                        console.log(`random: ${randomNumber}`);
                         numbersSorteados.push(randomNumber);
                     }
             
                     this.setState({numbersSorteados});
+                    console.log(numbersSorteados);
     
                 } else {
     
-                    for(var i = 0; i < quantity; i++){
+                    for(i = 0; i < quantity; i++){
                     
                         randomNumber = Math.floor(Math.random() * (end + 1 - start));
                         randomNumber += start;
                         numbersSorteados.push(randomNumber);
                         this.setState({numbersSorteados});
                     }
-    
                 }
-    
             });
-
         }
     }
 
-   
+    error = () => {
 
-    handleSubmit = () =>{
+        const {noRepeatError, rangeError, valueError} = this.state;
 
-        // use this function on the buttons to call the randomNumbers or randomLetters and etc
-        // change the validation logic. Think of a better way to do it
-        // because the  handle input shouldn't validate, I guess and the calculate functions either
+        if(noRepeatError){
+
+            return(
+                <div className="row">
+
+                    <div>
+                        <p className="error" id="regra3-error">Impossível sortear números únicos com essa faixa e quantidade</p>
+                    </div>
+
+                </div>
+            )
+
+        } else if (rangeError){
+
+            console.log("aqui");
+            return(
+                <div className="row">
+
+                    <div>
+                        <p className="error" id="regra3-error">Impossível sortear números com faixa menor ou igual a 0</p>
+                    </div>
+
+                </div>
+            )
+
+        } else if (valueError){
+
+            return(
+                <div className="row">
+
+                    <div>
+                        <p className="error" id="regra3-error">Valores inválidos. valores tem que ser números inteiros</p>
+                    </div>
+
+                </div>
+            )
+        } 
+        else {
+
+            return(
+                <div className='temp-separator' id='sorteio-separador'></div>
+            )
+        }
+
     }
 
+    validate = () => {
 
+        let {start, end, quantity, unique} = this.state;
+        let noRepeatError = this.state.noRepeatError;
+        let rangeError = this.state.rangeError;
+        let valueError = this.state.valueError;
+        let error = false;
+
+        if(!isNaN(start) && !isNaN(end) && !isNaN(quantity)){
+
+            // when the range is <= 0
+            quantity = parseInt(quantity);
+            start = parseInt(start);
+            end = parseInt(end);
+
+            if(start > end || start === end){
+
+                console.log("range error");
+                this.setState({rangeError: true});
+                rangeError = true;
+                error = true;
+            } 
+
+            // if is impossible to calculate unique numbers
+            else if(unique === "yes" && quantity > (end - start + 1) ){
+
+                console.log("noRepeatError");
+                this.setState({noRepeatError: true});
+                noRepeatError = true;
+                error = true;
+            }
+
+        } else {
+            console.log("not a number");
+            valueError = true;
+            this.setState({valueError: true});
+            error = true;
+        }
+
+        return error;
+    }
+   
+    handleSubmit = option => {
+
+        switch(option){
+
+            case 1:
+                this.randomNumbers();
+                break;
+            case 2:
+                this.randomLetters();
+                break;
+            default:
+                break;
+        }
+    }
+ 
     handleOption = e => {
 
         this.setState({unique: e.target.value});
     }
 
     // change to OnChange
-    handleInput = e => {
-
-        // make a validation function to validate those things I think
-        // or maybe change the other components to not allow type wrong values either
-        // maybe change to allow negative numbers because it doesn't allow if testing for isNan
-        // maybe change all to the onfocusout event so it won't trigger when typing "-" to type a negative number
-        // or make a exception just for the "-"
-        if(!isNaN(e.target.value) && e.target.value !== 0){
-
-            this.setState({[e.target.name] : e.target.value});
-        }
+    onChange = e => {
+        
+        this.setState({[e.target.name] : e.target.value});
+        
     }
 
     render(){
@@ -122,12 +200,12 @@ export default class Sorteio extends React.Component {
                     <div className="box-content" id="box-content">
                         <h2 className="box-title">Sorteador</h2>
                             <div className="row"> 
-                                <h3 className="subtitle">Sortear Número {this.state.noRepeatError? "true": "false"} </h3>
+                                <h3 className="subtitle">Sortear Número {this.state.start} - {this.state.end}  </h3>
                             </div>
                             <div className="full-row">
                                 <div className="sorteio-numeros-row">
                                     <div className="sorteio-numeros-block">
-                                        <div className="forms-title">Faixa</div>
+                                        <div className="forms-title">Faixa (incluindo ambos)</div>
                                         <div id="range">
                                             <div id="range-start">
                                                 <input
@@ -136,7 +214,7 @@ export default class Sorteio extends React.Component {
                                                     name="start" 
                                                     placeholder="inicio"
                                                     value={this.state.start}
-                                                    onChange={this.handleInput}
+                                                    onChange={this.onChange}
                                                 />
                                             </div>
 
@@ -151,7 +229,7 @@ export default class Sorteio extends React.Component {
                                                     name="end" 
                                                     placeholder="fim"
                                                     value={this.state.end}
-                                                    onChange={this.handleInput}
+                                                    onChange={this.onChange}
                                                 />
                                             </div>
                                         </div>
@@ -189,7 +267,7 @@ export default class Sorteio extends React.Component {
                                                 name="quantity" 
                                                 placeholder="quantidade"
                                                 value={this.state.quantity}
-                                                onChange={this.handleInput}
+                                                onChange={this.onChange}
                                             />
                                          </div>
                                     </div>
@@ -198,7 +276,7 @@ export default class Sorteio extends React.Component {
 
                             </div>
                             <div className="sortear-row">
-                                <div className="button sorteio-button sorteio-button" onClick={this.randomNumbers}>Sortear</div>
+                                <div className="button sorteio-button sorteio-button" onClick={this.handleSubmit.bind(this, 1)}>Sortear</div>
                                 <div className="title sorteio-title">Números sorteados: </div>
                                 <div className="results">
                                 {
@@ -206,6 +284,9 @@ export default class Sorteio extends React.Component {
                                 }
                                 </div>
                             </div>
+                            {
+                                this.error()
+                            }
                     </div>
                 </div>
             </div>
